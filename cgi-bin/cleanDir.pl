5037e15ccde196da8c0560e4e46338cb5b91b2be
Wenjie Deng <wdeng2@fredhutch.org>
2025-03-28 15:09:17 -0700

    modified cleanDir.pl to take care of dead jobs

--- cgi-bin/cleanDir.pl
+++ cgi-bin/cleanDir.pl
@@ -8,25 +8,27 @@ opendir DIR, $dir;
 while (my $subdir = readdir DIR) {
 	next if $subdir =~ /^\./;
 	$subdir = $dir.'/'.$subdir;
-	my $rmflag = 0;
+	my $rmflag = 1;
 	if (-d $subdir) {
-		opendir SUBDIR, $subdir;
-		while (my $file = readdir SUBDIR) {
-			if ($file eq "toggle") {
-				my $togglefile = $subdir."/toggle";
-				if((-f $togglefile) && (-M $togglefile > 5)) {
-					$rmflag = 1;
+		my $togglefile = $subdir."/toggle";
+		if(-e $togglefile) { # job finished
+			if (-M $togglefile < 5) {
+				$rmflag = 0;
+			}
+		}else { # job is running or failed for some reasons
+			opendir SUBDIR, $subdir;
+			while (my $file = readdir SUBDIR) {
+				my $fullname = $subdir."/".$file;
+				if((-f $fullname) && (-M $fullname < 5)) {
+					$rmflag = 0;
 					last;
 				}
 			}
+			closedir SUBDIR;
 		}
-		closedir SUBDIR;
 		if ($rmflag) {
 			rmtree($subdir);
-			#print "$subdir has been removed.\n";
 		}
 	}	
 }
 closedir DIR;
-
-
